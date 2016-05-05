03091b4891820529efe2bc2a0d595b8075402f40
Ada Ge <ada.ge@husqvarnagroup.com>
2016-05-05 21:31:45 +0200

    Change es6 syntax

--- player.js
+++ player.js
@@ -9,16 +9,16 @@ const VALUE_BLACK_JACK = 21;
 const NUMBER_DEALER_CARDS = 2;
 
 
-const _isCardAce = (card) => {
+const _isCardAce = function (card)  {
   return card === "A";
-}
+};
 
-const _isCardFace = (card) => {
+const _isCardFace = function (card) {
   return isNaN(card) && !_isCardAce(card);
-}
+};
 
-const _getCardValue = (card, countAceSmaller) => {
-  if(_isCardAce(card) {
+const _getCardValue = function (card, countAceSmaller) {
+  if(_isCardAce(card)){
     return countAceSmaller ? ACE_VALUE_SMALL : ACE_VALUE_BIG;
   }
  if(_isCardFace(card)) {
@@ -27,39 +27,41 @@ const _getCardValue = (card, countAceSmaller) => {
  return parseInt(card);
 };
 
-const _getMaxValueOfCards = (cards) => {
-  let sum = 0;
-  for (let i = 0; i< cards.length; i++) {
+const _getMaxValueOfCards = function (cards)  {
+  var sum = 0;
+  for (var i = 0; i< cards.length; i++) {
       sum = sum + _getCardValue(cards[i], false);
   }
   return sum;
 };
 
-const _getMinValueOfCards = (cards) => {
-  let sum = 0;
-  for (let i = 0; i< cards.length; i++) {
+const _getMinValueOfCards = function (cards)  {
+  var sum = 0;
+  for (var i = 0; i< cards.length; i++) {
       sum = sum + _getCardValue(cards[i], true);
   }
   return sum;
 };
 
-const _getNumberOfAvailableCardSmallerThanValue = (value, playerCards, dealerCard) => {
+const _getNumberOfAvailableCardSmallerThanValue = function (value, playerCards, dealerCard) {
     const nbTotalCards = value > 10 ? (10 - 1) * 4 : (value - 1) * 4;
     // number of cards smaller than the value that are taken by player
-    const nbPlayerTakenCards = playerCards.filter((card)=> _getCardValue(card, true) < value).length;
+    const nbPlayerTakenCards = playerCards.filter(function(card){
+      return _getCardValue(card, true) < value;
+    }).length;
     // number of card smaller than the value that is taken by dealer
     const nbDealerTakenCard = dealerCard < value ? 1 : 0;
     const nbDealerUnkownCard = 1;
     return nbTotalCards - nbPlayerTakenCards - nbDealerTakenCard - nbDealerUnkownCard;
-}
+};
 
-const _getIsPlayerSafeToHit = (value, playerCards, dealderCard) => {
+const _getIsPlayerSafeToHit = function (value, playerCards, dealerCard)  {
     const nbUnusedCards = TOTAL_CARDS - playerCards.length - NUMBER_DEALER_CARDS;
     const nbUnusedCardsSmallerThanValue = _getNumberOfAvailableCardSmallerThanValue(value, playerCards, dealderCard);
     return nbUnusedCardsSmallerThanValue / nbUnusedCards > 0.5;
-}
+};
 
-const play = (playerCards, dealderCard) => {
+const play = function(playerCards, dealerCard) {
   // check if player has black jack
   const playerMaxValue = _getMaxValueOfCards(playerCards);
   if(playerMaxValue == VALUE_BLACK_JACK) {
@@ -75,5 +77,7 @@ const play = (playerCards, dealderCard) => {
   return STAND;
 };
 
-
+// const playerCards = ["1", "1", "1", "1"];
+// const dealderCard = "8";
+// console.log("result=" + play(playerCards, dealderCard));
 play(playerCards, dealderCard);
