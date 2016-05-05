56ca2981cb6199e053c285702448911b4335364f
Ada Ge <ada.ge@husqvarnagroup.com>
2016-05-05 22:41:19 +0200

    Indent code

--- basic.js
+++ basic.js
@@ -1,40 +1,40 @@
 module.exports = {
-    HIT : "HIT",
-    STAND : "STAND",
-    ACE_VALUE_SMALL : 1,
-    ACE_VALUE_BIG : 11,
-    VALUE_FOR_FACE_CARD: 10,
-    TOTAL_CARDS : 52,
-    VALUE_BLACK_JACK: 21,
-    _isCardAce : function (card)  {
-      return card === "A";
-    },
-    _isCardFace : function (card) {
-      return isNaN(card) && !_isCardAce(card);
-    },
-    _getCardValue : function (card, countAceSmaller) {
-      if(this._isCardAce(card)){
-        return countAceSmaller ? this.ACE_VALUE_SMALL : this.ACE_VALUE_BIG;
-      }
-     if(this._isCardFace(card)) {
-       return this.VALUE_FOR_FACE_CARD;
-     }
-     return parseInt(card);
-   },
-    _getMaxValueOfCards : function (cards)  {
-         var sum = 0;
-         for (var i = 0; i< cards.length; i++) {
-             sum = sum + this._getCardValue(cards[i], false);
-         }
-         return sum;
+  HIT : "HIT",
+  STAND : "STAND",
+  ACE_VALUE_SMALL : 1,
+  ACE_VALUE_BIG : 11,
+  VALUE_FOR_FACE_CARD: 10,
+  TOTAL_CARDS : 52,
+  VALUE_BLACK_JACK: 21,
+  _isCardAce : function (card)  {
+    return card === "A";
   },
-  _getMinValueOfCards : function (cards)  {
+  _isCardFace : function (card) {
+    return isNaN(card) && !_isCardAce(card);
+  },
+  _getCardValue : function (card, countAceSmaller) {
+    if(this._isCardAce(card)){
+      return countAceSmaller ? this.ACE_VALUE_SMALL : this.ACE_VALUE_BIG;
+    }
+    if(this._isCardFace(card)) {
+      return this.VALUE_FOR_FACE_CARD;
+    }
+    return parseInt(card);
+  },
+  _getValueOfCards: function(cards, minOrMax) {
     var sum = 0;
     for (var i = 0; i< cards.length; i++) {
-        sum = sum + _getCardValue(cards[i], true);
+      sum = sum + this._getCardValue(cards[i], minOrMax);
     }
     return sum;
   },
+  _getMaxValueOfCards : function (cards)  {
+    return _getValueOfCards(false);
+  },
+  _getMinValueOfCards : function (cards)  {
+    return _getValueOfCards(true);
+  },
+
   _hasBlackJack: function(cards) {
     return this._getMaxValueOfCards(cards) === this.VALUE_BLACK_JACK;
   }
